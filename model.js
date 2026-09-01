62b325cfe93f916bbf6dbe24bc74e2933928ef1d
praveen <patipraveen01@gmail.com>
2026-09-01 11:08:04 +0530

    database

--- model.js
+++ model.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+const UserData = mongoose.Schema({
 
-const userDB  = mongoose.Schema({
-    username:{
+      username :{
+        type : String,
+        required : true
+      },
+      email:{
         type:String,
-        require: true
-    },
-    
+        required:true
+      },
+      Password :{
+        type:String,
+        required:true
+      },
+      CreatedAt:{
+        type:Date,
+        default: Date.now
+      }
+
 })
+
+module.exports = mongoose.model("userdata", UserData)
